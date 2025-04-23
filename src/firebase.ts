7c8402148044aa06f5986bfc2cd2c7bb335a4829
NikolausA <nik.aleksenko@gmail.com>
2025-04-23 11:12:58 +0300

    add .env file

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAbHHEtqlPTkHG84rhqwBqZluq6B1cOun0',
-  authDomain: 'notesapp-7c7aa.firebaseapp.com',
-  projectId: 'notesapp-7c7aa',
-  storageBucket: 'notesapp-7c7aa.firebasestorage.app',
-  messagingSenderId: '987165782502',
-  appId: '1:987165782502:web:d1636ab279dd2813126627',
-  databaseURL: 'https://notesapp-7c7aa-default-rtdb.europe-west1.firebasedatabase.app/',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
 };
 
 const app = initializeApp(firebaseConfig);
