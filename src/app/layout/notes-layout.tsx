45df89ae0dcfcef162429aa0bcb885dc2f87f6f7
NikolausA <nik.aleksenko@gmail.com>
2025-05-16 18:36:32 +0300

    feat: implements note deleting

--- src/app/layout/notes-layout.tsx
+++ src/app/layout/notes-layout.tsx
@@ -1,9 +1,19 @@
 import { useEffect, useState } from 'react';
 import { TbDeviceFloppy, TbEdit, TbLogout, TbPlus, TbSearch, TbTrash } from 'react-icons/tb';
 import { useNavigate } from 'react-router-dom';
-import { ActionIcon, AppShell, Burger, Flex, Text, TextInput } from '@mantine/core';
+import {
+  ActionIcon,
+  AppShell,
+  Burger,
+  Button,
+  Flex,
+  Group,
+  Modal,
+  Text,
+  TextInput,
+} from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
-import { useFetchNotes } from '@/entities';
+import { useDeleteNote, useFetchNotes } from '@/entities';
 import { useAuth } from '../';
 import { filterNotesBySearch, NoteData, useDebounce } from '../../shared';
 import { NoteCard, NoteEditor, NotePlaceholder, NoteViewer } from '../../widgets';
@@ -15,7 +25,9 @@ export const NotesLayout = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [opened, { toggle }] = useDisclosure();
   const { notes, isLoading } = useFetchNotes();
+  const requestDeleteNote = useDeleteNote();
   const [filtredNotes, setFiltredNotes] = useState<Array<[string, NoteData]>>([]);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
 
   useEffect(() => {
     setFiltredNotes(filterNotesBySearch(notes, searchQuery));
@@ -29,8 +41,6 @@ export const NotesLayout = () => {
     notes,
     searchQuery,
   ]);
-
-  // console.log(filtredNotes);
 
   const { user, signout } = useAuth();
   const navigate = useNavigate();
@@ -52,125 +62,157 @@ export const NotesLayout = () => {
     setIsEditing(false);
   };
 
+  const handleDelete = () => {
+    if (!selectedNoteId) {
+      return;
+    }
+    requestDeleteNote(selectedNoteId);
+    setSelectedNoteId(null);
+    setIsEditing(false);
+    setIsDeleteModalOpen(false);
+  };
+
   const handleLogout = () => {
     signout(() => navigate('/login'));
   };
 
   return (
-    <AppShell
-      header={{ height: 60, offset: true }}
-      navbar={{
-        width: 300,
-        breakpoint: 'sm',
-        collapsed: { mobile: !opened },
-      }}
-      padding="md"
-    >
-      <AppShell.Header withBorder>
-        <Flex h="100%" align="center" p="md">
-          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+    <>
+      <Modal
+        opened={isDeleteModalOpen}
+        onClose={() => setIsDeleteModalOpen(false)}
+        title="Удалить заметку?"
+        centered
+      >
+        <Text>Вы уверены, что хотите удалить эту заметку? Это действие необратимо.</Text>
+        <Group justify="flex-end" mt="md">
+          <Button variant="default" onClick={() => setIsDeleteModalOpen(false)}>
+            Отмена
+          </Button>
+          <Button color="red" onClick={handleDelete}>
+            Удалить
+          </Button>
+        </Group>
+      </Modal>
+      <AppShell
+        header={{ height: 60, offset: true }}
+        navbar={{
+          width: 300,
+          breakpoint: 'sm',
+          collapsed: { mobile: !opened },
+        }}
+        padding="md"
+      >
+        <AppShell.Header withBorder>
+          <Flex h="100%" align="center" p="md">
+            <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
 
-          <Flex justify="center" align="center" style={{ flex: 1 }} gap="sm">
-            {selectedNote && (
-              <>
-                <ActionIcon variant="subtle" aria-label="Удалить">
-                  <TbTrash />
-                </ActionIcon>
-                <ActionIcon onClick={handleEdit} variant="subtle" aria-label="Редактировать">
-                  {isEditing ? <TbDeviceFloppy /> : <TbEdit />}
-                </ActionIcon>
-              </>
-            )}
+            <Flex justify="center" align="center" style={{ flex: 1 }} gap="sm">
+              {selectedNote && (
+                <>
+                  <ActionIcon
+                    onClick={() => setIsDeleteModalOpen(true)}
+                    variant="subtle"
+                    aria-label="Удалить"
+                  >
+                    <TbTrash />
+                  </ActionIcon>
+                  <ActionIcon onClick={handleEdit} variant="subtle" aria-label="Редактировать">
+                    {isEditing ? <TbDeviceFloppy /> : <TbEdit />}
+                  </ActionIcon>
+                </>
+              )}
+            </Flex>
+            <Flex align="center" gap="md">
+              <TextInput
+                placeholder="Поиск заметок..."
+                variant="filled"
+                radius="md"
+                size="sm"
+                leftSection={<TbSearch size="1rem" />}
+                value={searchQuery}
+                style={{ width: 200 }}
+                onChange={handleSearch}
+              />
+
+              {user && (
+                <Flex align="center" gap="sm">
+                  <Text fw={500}>{user.name}</Text>
+                  <ActionIcon variant="subtle" onClick={handleLogout}>
+                    <TbLogout />
+                  </ActionIcon>
+                </Flex>
+              )}
+            </Flex>
           </Flex>
-          <Flex align="center" gap="md">
-            <TextInput
-              placeholder="Поиск заметок..."
-              variant="filled"
-              radius="md"
-              size="sm"
-              leftSection={<TbSearch size="1rem" />}
-              value={searchQuery}
-              style={{ width: 200 }}
-              onChange={handleSearch}
-            />
+        </AppShell.Header>
 
-            {user && (
-              <Flex align="center" gap="sm">
-                <Text fw={500}>{user.name}</Text>
-                <ActionIcon variant="subtle" onClick={handleLogout}>
-                  <TbLogout />
-                </ActionIcon>
-              </Flex>
-            )}
+        <AppShell.Navbar p="md" withBorder>
+          <Flex justify="center" mb="sm">
+            <ActionIcon
+              onClick={handleCreateNewNote}
+              bg="dark.6"
+              style={{
+                width: '60%',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+              size="lg"
+            >
+              <TbPlus size="1.5rem" />
+            </ActionIcon>
           </Flex>
-        </Flex>
-      </AppShell.Header>
 
-      <AppShell.Navbar p="md" withBorder>
-        <Flex justify="center" mb="sm">
-          <ActionIcon
-            onClick={handleCreateNewNote}
-            bg="dark.6"
-            style={{
-              width: '60%',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
-            size="lg"
-          >
-            <TbPlus size="1.5rem" />
-          </ActionIcon>
-        </Flex>
-
-        {isLoading ? (
-          <div>Loading...</div>
-        ) : (
-          filtredNotes.map(([id, { title, content, createdAt }]) => {
-            const isSelected = id === selectedNoteId;
-
-            return (
-              <button
-                key={id}
-                type="button"
-                onClick={() => handleSelectNote(id)}
-                style={{
-                  all: 'unset',
-                  cursor: 'pointer',
-                  width: '100%',
-                  textAlign: 'left',
-                  backgroundColor: isSelected ? '#2C2E33' : 'transparent',
-                  borderRadius: '8px',
-                  padding: '8px',
-                }}
-              >
-                <NoteCard title={title} content={content} createdAt={createdAt} />
-              </button>
-            );
-          })
-        )}
-      </AppShell.Navbar>
-      <AppShell.Main pt={80}>
-        {isCreating ? (
-          <NoteEditor
-            id=""
-            selectedNote={{ title: '', content: '' }}
-            setIsCreating={setIsCreating}
-          />
-        ) : selectedNote ? (
-          isEditing ? (
-            <NoteEditor id={selectedNoteId!} selectedNote={selectedNote} />
+          {isLoading ? (
+            <div>Loading...</div>
           ) : (
-            <NoteViewer
-              title={selectedNote.title}
-              content={selectedNote.content}
-              createdAt={selectedNote.createdAt}
+            filtredNotes.map(([id, { title, content, createdAt }]) => {
+              const isSelected = id === selectedNoteId;
+
+              return (
+                <button
+                  key={id}
+                  type="button"
+                  onClick={() => handleSelectNote(id)}
+                  style={{
+                    all: 'unset',
+                    cursor: 'pointer',
+                    width: '100%',
+                    textAlign: 'left',
+                    backgroundColor: isSelected ? '#2C2E33' : 'transparent',
+                    borderRadius: '8px',
+                    padding: '8px',
+                  }}
+                >
+                  <NoteCard title={title} content={content} createdAt={createdAt} />
+                </button>
+              );
+            })
+          )}
+        </AppShell.Navbar>
+        <AppShell.Main pt={80}>
+          {isCreating ? (
+            <NoteEditor
+              id=""
+              selectedNote={{ title: '', content: '' }}
+              setIsCreating={setIsCreating}
             />
-          )
-        ) : (
-          <NotePlaceholder />
-        )}
-      </AppShell.Main>
-    </AppShell>
+          ) : selectedNote ? (
+            isEditing ? (
+              <NoteEditor id={selectedNoteId!} selectedNote={selectedNote} />
+            ) : (
+              <NoteViewer
+                title={selectedNote.title}
+                content={selectedNote.content}
+                createdAt={selectedNote.createdAt}
+              />
+            )
+          ) : (
+            <NotePlaceholder />
+          )}
+        </AppShell.Main>
+      </AppShell>
+    </>
   );
 };
--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,3 +1,4 @@
 export * from './note/model/use-fetch-notes';
 export * from './note/model/use-update-note';
 export * from './note/model/use-add-note';
+export * from './note/model/use-delete-note';
